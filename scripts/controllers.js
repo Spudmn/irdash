ea118d3d89cd19555e39887911025f7e4c0f0dc5
Pierre Minnieur <pm@pierre-minnieur.de>
2016-04-30 12:19:33 +0200

    finished responsive refactoring

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -26,42 +26,6 @@
         $scope.max     = 0
         $scope.drivers = 0
 
-        setTimeout(function() {
-            $scope.ir.PlayerCarPosition = 1
-            $scope.ir.FuelLevel = 57.8
-            $scope.ir.Gear = 4
-            $scope.ir.Speed = 23.5
-            $scope.ir.dcABS = 4
-            $scope.ir.dcThrottleShape = 10
-            $scope.ir.dcTractionControl = 8
-            $scope.ir.WaterTemp = 89.9
-            $scope.ir.OilPress = 7
-            $scope.ir.OilTemp = 89.9
-            $scope.ir.dcBrakeBias = 49.8
-            $scope.ir.Voltage = 13.9
-            $scope.ir.Lap = 3
-            $scope.ir.LapLastLapTime = 236.3772
-            $scope.ir.LapBestLapTime = 238.8743
-            $scope.ir.SessionNum = 1
-            $scope.ir.SessionInfo = {
-                Sessions: {
-                    1: { SessionLaps: 7 }
-                }
-            }
-            $scope.ir.EngineWarnings =
-                WATER_TEMP_WARNING |
-                FUEL_PRESSURE_WARNING |
-                OIL_PRESSURE_WARNING
-
-            $scope.ir.RPM = 7299
-            $scope.ir.LapDeltaToBestLap = -2.399
-            $scope.ir.DriverInfo = {
-                DriverCarRedLine: 9000,
-                DriverCarSLBlinkRPM: 8800,
-                Drivers: []
-            }
-        }, 500);
-
         $scope.$watch('ir.DriverInfo', function(n, o) {
             if (!n || null == n) {
                 return
